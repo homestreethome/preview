dad4bd083b91e4b6ab8dcf9ff031c339c07fb11b
Aaron Israel <me@a0n.name>
2014-09-15 01:04:34 +0200

    .

--- styles/home_components.min.css
+++ styles/home_components.min.css
@@ -1,6 +1,6 @@
 /**
  * audiowalk
- * @version v0.1.0 - 2014-09-13
+ * @version v0.1.0 - 2014-09-14
  * @link https://github.com/lennart/audiowalk
  * @author Lennart Melzer ()
  * @license MIT License, http://www.opensource.org/licenses/MIT
